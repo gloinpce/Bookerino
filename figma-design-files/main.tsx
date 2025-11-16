0c284fd65cf54af16573cac94b4a387e481f56a4
gloinpce <swagshawtyarchive@gmail.com>
2025-11-16 12:25:14 +0200

    fix: Add React import and comment to main.tsx reference file

--- figma-design-files/main.tsx
+++ figma-design-files/main.tsx
@@ -1,5 +1,14 @@
+/**
+ * NOTE: This is a reference file for Figma design purposes.
+ * These files are copies of the actual source code and are not meant to compile.
+ * TypeScript errors are expected as dependencies are not available in this folder.
+ * For the actual working code, see: client/src/website/main.tsx
+ */
+
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+// @ts-ignore - Reference file, dependencies not available
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
